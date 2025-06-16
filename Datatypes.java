017f3b4911e23dfbeb4165fa696d76596f5c6b70
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2025-06-16 20:45:21 +0300

    Task:Learnt about different types of variables

---
+++ Datatypes.java
@@ -0,0 +1,12 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        byte a = 101;
+        String name = "Baraka";
+        int age = 100;
+        double weight = 345.5;
+        char gender = 'm';
+        boolean isstraight = true;
+        float simpledecimal = 10.0;
+        short mfupi = 1000;
+        
+}
