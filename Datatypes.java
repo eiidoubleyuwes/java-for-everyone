2d732947f29275ba0b556055028c127fc89bc46d
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2025-06-16 21:42:57 +0300

    Task:Learnt about String conversion and also type casting

--- Datatypes.java
+++ Datatypes.java
@@ -1,12 +1,24 @@
 public class Datatypes {
     public static void main(String[] args) {
-        byte a = 101;
+        byte a = 2;
         String name = "Baraka";
         int age = 100;
         double weight = 345.5;
         char gender = 'm';
         boolean isstraight = true;
-        float simpledecimal = 10.0;
         short mfupi = 1000;
-        
+        System.out.println(a);
+        System.out.println("number"+20+5);
+
+        //String conversion;
+        int ea = 100;
+        String agge = String.valueof(ea);
+        double weiight = 1000.0001;
+        String uzito = String.valueof(weight);
+
+        // Convert explicitly
+        long mrefu = 1000000L;
+        int mfupikiasi = (int) mrefu;
+    }
+
 }
